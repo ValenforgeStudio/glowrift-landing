51d573814239f76200c0361bc1a4e9025d618455
ValenforgeStudio <michael@valenforgestudio.com>
2025-06-12 22:33:09 -0500

    Add footer with link to Valenforge Studio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
 content: ["./**/*.html"],
   theme: {
-    extend: {},
+  extend: {
+    colors: {
+      gold: '#E6C200',
+      silver: '#C0C0C0',
+      sunset: {
+        DEFAULT: '#D9A15E',
+        dark: '#A86C36',
+      },
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      display: ['Playfair Display', 'serif'],
+    },
   },
+},
   plugins: [],
 }
