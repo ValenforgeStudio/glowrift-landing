a4a1708ae6b31389c7129c054d902c1440c40f1a
ValenforgeStudio <michael@valenforgestudio.com>
2025-06-16 22:09:58 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -14,8 +14,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("dist");
   eleventyConfig.addPassthroughCopy({ "scripts": "scripts" });
-  eleventyConfig.addPassthroughCopy("scripts/env.js");
-
 
   // ✅ Serve 404.html in dev
   eleventyConfig.setBrowserSyncConfig({
